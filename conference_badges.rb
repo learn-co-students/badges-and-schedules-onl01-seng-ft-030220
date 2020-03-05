dd7b8c43f0bb3c0fb27d529f78d60995e0f4cb27
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 00:21:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+# ----------------------------------------------------------------
+
+def batch_badge_creator(array)
+  batch_messages = []
+  
+  array.each do |name|
+    batch_messages.push("Hello, my name is #{name}.")
+  end
+  
+  return batch_messages
+end
+
+# ----------------------------------------------------------------
+
+def assign_rooms(array)
+  assigned = []
+  counter = 1
+  
+  array.each do |name|
+    assigned.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  
+  return assigned
+end
+
+# ----------------------------------------------------------------
+
+def printer(array)
+  
+  batch_badge_creator(array).each do |element|
+    puts element
+  end 
+  
+  assign_rooms(array).each do |element|
+    puts element
+  end
+  
+end 
+
